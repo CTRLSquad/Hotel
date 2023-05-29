e33ac53da73a791f854c4b454f98fab108fe75ea
MaksSt <maksimsitnikov224@gmail.com>
2023-05-29 08:56:51 +0500

    1st commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Hotel.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace Hotel.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
