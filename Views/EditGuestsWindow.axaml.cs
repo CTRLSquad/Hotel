4fcee4d25fc7d9df2e41a2bbc312913d9f04a0ed
MaksSt <maksimsitnikov224@gmail.com>
2023-05-30 12:17:21 +0500

    add CRUD

---
+++ Views/EditGuestsWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Hotel.Views
+{
+    public partial class EditGuestsWindow : Window
+    {
+        public EditGuestsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
